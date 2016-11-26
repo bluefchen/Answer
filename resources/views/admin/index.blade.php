473e55d56c300bc6e30d7696d2c54e3dfb239685
siluzhou <1912317621@qq.com>
2016-11-26 11:31:20 +0800

    1.1.1
    admin index界面更新

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,11 +7,14 @@
         <div class="row">
             <div class="col-md-7 ">
                 <div class="row">
-                    <h2>题目列表 &nbsp <button type="submit" class="btn btn-lg btn-info">Add</button></h2>
-
+                    <h2>
+                        <a href="/admin/article/create" class="btn btn-lg" style="padding:0px 4px;font-size:40px">
+                            <span class="glyphicon glyphicon-plus "></span>
+                        </a>
+                        &nbsp
+                        题目列表
+                    </h2>
                 </div>
-
-
                 <div class="container col-md-12">
                     <form role="form">
                         <div class="form-group">
@@ -19,7 +22,7 @@
                                 <label class="checkbox ">
                                     <input type="checkbox" data-toggle="checkbox" value={{$question->id}}>
                                     <h4>
-                                        <a href="/admin/articles/{{$question->id}}">
+                                        <a href="/admin/article/{{$question->id}}">
                                             <strong>{{$question->id}}</strong>
                                             &nbsp;
                                             {{substr($question->title,0,50)}}
@@ -35,10 +38,21 @@
                         <button type="submit" class="btn btn-lg btn-primary">Delete</button>
                     </form>
                 </div>
+
             </div>
 
+
             <div class="col-md-4">
-                <h2>Tag列表 <button type="submit" class="btn btn-lg btn-info">Add</button></h2>
+                <div class="row">
+                    <h2>
+                        <a href="/admin/tag/create" class="btn btn-lg" style="padding:0px 4px;font-size:40px">
+                            <span class="glyphicon glyphicon-plus "></span>
+                        </a>
+                        &nbsp
+                        Tag列表
+                    </h2>
+                </div>
+
                 <div class="col-md-12 ">
                     <form role="form">
                         <div class="form-group">
@@ -46,8 +60,13 @@
                                 <label class="checkbox ">
                                     <input type="checkbox" data-toggle="checkbox" value={{$tag->id}}>
                                     <h4>
-                                        <a href="/admin/tags/{{$tag->id}}">
-                                            {{$tag->name}}
+                                        {{$tag->name}}
+                                        @for($i=0;$i<10-strlen($tag->name);$i++)
+                                            &emsp;
+                                        @endfor
+                                        <a href="/admin/tag/{{$tag->id}}/edit" class="btn btn-sm"
+                                           style="padding:0px 4px;font-size:20px">
+                                            <span class="glyphicon glyphicon-pencil "></span>
                                         </a>
                                     </h4>
                                 </label>
