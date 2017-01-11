4b15ac92a4c51d9fdd6a407f4a9c4e803940c288
siluzhou <1912317621@qq.com>
2017-01-11 11:31:19 +0800

    3.1.2.2
    微信公众号修改2

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -9,4 +9,47 @@ use App\Http\Requests;
 class WeixinController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->beforeFilter('weixin', array('on' => 'get|post'));
+    }
+
+    public function index(Request $request)
+    {
+        /*$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $RX_TYPE = trim($postObj->MsgType);
+            $result=$RX_TYPE;
+            echo $result;
+            //   $this->test($RX_TYPE) ;
+            /*switch ($RX_TYPE)
+            {
+                case "text":
+                    $resultStr = $this->receiveText($postObj);
+                    break;
+                case "image":
+                    $resultStr = $this->receiveImage($postObj);
+                    break ;
+                case "voice":
+                    $resultStr = $this->receiveVoice($postObj) ;
+                    break ;
+                case "event":
+                    $resultStr = $this->receiveEvent($postObj);
+                    break;
+                default:
+                    $resultStr = "unknow msg type: ".$RX_TYPE;
+                    break;
+            }
+            echo $resultStr;
+
+            }
+            else {
+            echo "";
+            exit;
+        }
+    */
+        return Input::get('echostr');
+
+    }
 }
---
+++ app/filters.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zhousilu
+ * Date: 2017/1/11
+ * Time: 11:21
+ */
+
+namespace App;
+
